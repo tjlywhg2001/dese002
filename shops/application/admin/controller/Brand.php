05fb3ff4a05fbbc73960a785ad6280584f175317
tjlywhg2001 <tjlywhg-2001@163.com>
2018-04-28 17:56:42 +0800

    20180428-1111

---
+++ shops/application/admin/controller/Brand.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace app\admin\controller;
+
+use Think\Controller;
+
+class Brand
+{
+
+	//列表
+	public function lst()
+	{
+
+		return view('list');
+
+	}
+
+
+	//添加
+	public function add()
+	{
+
+		return view();
+
+	}
+
+
+	//编辑
+	public function edit()
+	{
+
+		return view();
+
+	}
+
+
+	//删除
+	public function del()
+	{
+
+		return view();
+
+	}
+
+}
---
+++ shops/application/admin/controller/Index.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace app\admin\controller;
+
+// use Think\Controller;
+
+class Index
+{
+
+	public function index()
+	{
+
+		return view();
+
+	}
+}
